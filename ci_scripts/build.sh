6a66f1fba3d6399590eff633354c824315ba465e
HZQ <huangzhiquan@bigbang.games>
2020-03-27 16:44:10 +0800

    fix build.sh

--- ci_scripts/build.sh
+++ ci_scripts/build.sh
@@ -3,14 +3,16 @@ if [ $# != 1 ];then
     exit     
 fi  
  
-#UNITY程序的路径#
+UNITY程序的路径#
 UNITY_PATH=/Volumes/ExtSSD/UnityApp/2019.3.5f1/Unity.app/Contents/MacOS/Unity
  
-#游戏程序路径#
-PROJECT_PATH=/Volumes/ExtSSD/Project/my_ci_test
+游戏程序路径#
+export PROJECT_PATH=$(dirname $(dirname $0))
+
+echo $PROJECT_PATH
 echo "version = $version"
  
-#在Unity中构建apk#
+在Unity中构建apk#
 $UNITY_PATH -projectPath $PROJECT_PATH -executeMethod ProjectBuild.BuildForAndroid version-"$version" -quit
  
 echo "Apk生成完毕"
