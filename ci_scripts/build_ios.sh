3c40cbd12f57117390ff33818515bdc0c10e9d84
HZQ <huangzhiquan@bigbang.games>
2020-03-30 11:47:54 +0800

    add ios build

---
+++ ci_scripts/build_ios.sh
@@ -0,0 +1,16 @@
+ echo "start"  
+ 
+#UNITY程序的路径
+UNITY_PATH=/Volumes/ExtSSD/UnityApp/2019.3.5f1/Unity.app/Contents/MacOS/Unity
+ 
+#游戏程序路径
+export PROJECT_PATH=$(dirname $(dirname $0))
+
+echo $PROJECT_PATH
+
+export logPath=$(dirname $(dirname $0))/BuildLog/last_build_ios.log
+
+#在Unity中构建apk
+$UNITY_PATH -quit -batchmode -projectPath $PROJECT_PATH -executeMethod ProjectBuild.BuildForIos -logFile $logPath
+ 
+echo "Apk生成完毕"
