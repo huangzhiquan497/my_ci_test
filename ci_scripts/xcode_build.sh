1d24c43d13a86b37a4938c2d379b46a421aadb5a
HZQ <huangzhiquan@bigbang.games>
2020-04-20 19:02:58 +0800

    add profile

--- ci_scripts/xcode_build.sh
+++ ci_scripts/xcode_build.sh
@@ -1,5 +1,5 @@
-#M_PROVISIONING_PROFILE="a8efop6a-33pt-5522-pokq-52d6d800aubv"#证书配置
-#M_CODE_SIGN_IDENTITY="iPhone Distribution: XXXXXX Technology INC. (XXXXXX)"#证书配置
+M_PROVISIONING_PROFILE="71ccbdf6-4562-451e-be64-11487f52fff4" #证书配置
+M_CODE_SIGN_IDENTITY="iPhone Distribution: TECHNOLOGY IN PLAY LTD (K36DYL578K)" #证书配置
 export PROJECT_PATH=$(dirname $(dirname $0))
 M_XCODE_PATH=${PROJECT_PATH}/BuildIpa/hzq_ci_test  #XCODE工程目录路径
 M_XCODE_NAME="Unity-iPhone"
@@ -7,7 +7,6 @@ M_ARCHIVE_PATH=${PROJECT_PATH}/archive
 M_EXPORT_PATH=${PROJECT_PATH}/ipa
 cd ${M_XCODE_PATH}
 
-xcodebuild clean archive -scheme ${M_XCODE_NAME} -target ${M_XCODE_NAME} -archivePath ${M_ARCHIVE_PATH} -configuration Release -allowProvisioningUpdates
-#PROVISIONING_PROFILE="${M_PROVISIONING_PROFILE}" CODE_SIGN_IDENTITY="${M_CODE_SIGN_IDENTITY}"
+xcodebuild clean archive -scheme ${M_XCODE_NAME} -target ${M_XCODE_NAME} -archivePath ${M_ARCHIVE_PATH} -configuration Release PROVISIONING_PROFILE="${M_PROVISIONING_PROFILE}" CODE_SIGN_IDENTITY="${M_CODE_SIGN_IDENTITY}"
 
 xcodebuild -exportArchive -archivePath "${M_ARCHIVE_PATH}.xcarchive" -exportPath "${M_EXPORT_PATH}" -exportOptionsPlist "Info.plist" -allowProvisioningUpdates
